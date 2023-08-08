45e062f9f6292071b92320132303c0ab9d4e816a
Rodrigo Farah <digofarah@gmail.com>
2023-08-08 11:27:58 -0300

    'Add' button working fine, creating a txt file

--- password_manager/main.py
+++ password_manager/main.py
@@ -4,7 +4,13 @@ import tkinter as tk
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-
+def save():
+    entries = [website_entry.get(), email_user_entry.get(), password_entry.get()]
+    item_to_add = " | ".join(entries) + "\n"
+    with open("password_manager/logfile.txt", "a") as logfile:
+        logfile.writelines(item_to_add)
+    website_entry.delete(0, tk.END)
+    password_entry.delete(0, tk.END)
 # ---------------------------- UI SETUP ------------------------------- #
 
 
@@ -22,13 +28,15 @@ canvas.grid(column=1, row=0)
 website_label = tk.Label(text="Website:", fg="black", bg="white")
 website_label.grid(column=0, row=1)
 website_entry = tk.Entry(width=35)
-website_entry.grid(column=1, row=1, columnspan=2, sticky="nsew")
+website_entry.grid(column=1, row=1, columnspan=2, sticky="ew")
+website_entry.focus()
 
 # E-mail/User Layout
 email_user_label = tk.Label(text="Email/Username:", fg="black", bg="white")
 email_user_label.grid(column=0, row=2)
 email_user_entry = tk.Entry(width=35)
-email_user_entry.grid(column=1, row=2, columnspan=2, sticky="nsew")
+email_user_entry.insert(0, "digofarah@gmail.com")
+email_user_entry.grid(column=1, row=2, columnspan=2, sticky="ew")
 
 # Password Layout
 password_label = tk.Label(text="Password:", fg="black", bg="white")
@@ -37,9 +45,9 @@ password_entry = tk.Entry(width=21)
 password_entry.grid(column=1, row=3, sticky="nsew")
 
 # Buttons Layout
-generate_button = tk.Button(text="Generate Password", fg="black", bg="white")
+generate_button = tk.Button(text="Generate Password", font=("Arial", 10), fg="black", bg="white", height=1)
 generate_button.grid(column=2, row=3, sticky="nsew")
-add_button = tk.Button(text="add", width=32, fg="black", bg="white")
+add_button = tk.Button(text="add", font=("Arial", 10), width=32, fg="black", bg="white", command=save)
 add_button.grid(column=1, row=4, columnspan=2, sticky="nsew")
 
 window.mainloop()
