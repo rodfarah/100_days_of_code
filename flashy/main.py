652976f4b871c0efbc837311ff8232391587a13e
Rodrigo Farah <digofarah@gmail.com>
2023-08-17 09:34:31 -0300

    Second Commit. Good solution. Json file not implemented yet.

--- flashy/main.py
+++ flashy/main.py
@@ -18,8 +18,22 @@ chosen_word = ""
 
 # ---------------------- Program Setup -------------------------
 
+
 def hit_start_first():
-    mb.showwarning(title="Give a try!", message="Please, hit start/stop button first!")
+    mb.showwarning(title="Give a try!",
+                   message="Please, hit start/stop button first!")
+
+def change_texts_french():
+    global upper_text, lower_text
+    card.itemconfig(actual_card, image=frontc_image)
+    card.itemconfig(upper_text, text="French", fill="black")
+    card.itemconfig(lower_text, text=choice(french_list), fill="black")
+
+def change_texts_english():
+    global chosen_word
+    card.itemconfig(actual_card, image=backc_image)
+    card.itemconfig(upper_text, text="English", fill="white")
+    card.itemconfig(lower_text, text=f"{words_dict[chosen_word]}", fill="white")
 
 def start_stop():
     if len(french_list) == 0:
@@ -37,34 +51,29 @@ def start_stop():
         global chosen_word
         running = True
         chosen_word = choice(french_list)
-        card.itemconfig(upper_text, text="French")
-        card.itemconfig(lower_text, text=chosen_word)
-
-        # Set changes after 3 seconds:
-        window.after(3000, lambda: card.create_image(400, 265, image=backc_back))
-        window.after(3000, lambda: card.itemconfig(upper_text, text="English", fill="white"))
-        window.after(3000, lambda: card.itemconfig(lower_text, text=f"{words_dict[chosen_word]}", fill="white"))
+        change_texts_french()
         
+        # Fliping card after 3 seconds:
+        window.after(3000, change_texts_english)
+
 
 def check_button_clicked():
-    if card.cget("text") == "Flashy Test":
+    global running
+    if not running:
         hit_start_first()
     else:
-        global running
         global chosen_word
         french_list.remove(chosen_word)
         running = False
-        card.create_image(400, 265, image=frontc_back)
         start_stop()
 
 
 def cross_button_clicked():
-    if len(french_list) == initial_len:
+    global running
+    if not running:
         hit_start_first()
     else:
-        global running
         running = False
-        card.create_image(400, 265, image=frontc_back)
         start_stop()
 
 
@@ -76,34 +85,34 @@ window.title("Flashy")
 window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 
 # Creating Canvas
-frontc_back = tk.PhotoImage(file="flashy/images/card_front.png")
-backc_back = tk.PhotoImage(file="flashy/images/card_back.png")
+frontc_image = tk.PhotoImage(file="flashy/images/card_front.png")
+backc_image = tk.PhotoImage(file="flashy/images/card_back.png")
 
 card = tk.Canvas(width=800, height=526, highlightthickness=0,
                  background=BACKGROUND_COLOR)
-card.create_image(400, 265, image=frontc_back)
+actual_card = card.create_image(400, 265, image=frontc_image)
 card.grid(column=0, row=0, columnspan=3)
 
 # Creating Texts
 upper_text = card.create_text(400, 150,
-    text="Flashy Test", 
-    fill="black",
-    font=("Ariel", 40, "bold")
-)
+                              text="Flashy Test",
+                              fill="black",
+                              font=("Ariel", 40, "bold"))
 lower_text = card.create_text(400, 263,
-    text="Hit 'Start/Stop' button below!", 
-    fill= "black",
-    font=("Ariel", 20, "italic")
-)
+                              text="Hit 'Start/Stop' button below!",
+                              fill="black",
+                              font=("Ariel", 20, "italic"))
 
 
 # Creating Buttons
 cross_image = tk.PhotoImage(file="flashy/images/wrong.png")
-cross_button = tk.Button(window, image=cross_image, command=cross_button_clicked)
+cross_button = tk.Button(window, image=cross_image,
+                         command=cross_button_clicked)
 cross_button.grid(column=0, row=1)
 
 check_image = tk.PhotoImage(file="flashy/images/right.png")
-check_button = tk.Button(window, image=check_image, bg="white", highlightthickness=0, command=check_button_clicked)
+check_button = tk.Button(window, image=check_image, bg="white",
+                         highlightthickness=0, command=check_button_clicked)
 check_button.grid(column=2, row=1)
 
 running = False
