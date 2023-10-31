b089b4154a422710b58de61cd2b65598e0dd9728
Rodrigo Farah <digofarah@gmail.com>
2023-10-31 15:03:10 -0300

    First commit. I found a very clever way to solve this exercise, by myself. Web scrapping using selenium.

---
+++ day_48/python_org_upcome_events.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option(name="detach", value=True)
+
+
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get(url="https://www.python.org/")
+
+events = [event.text.split("\n") for event in driver.find_elements(By.CSS_SELECTOR, value=".event-widget li")]
+
+result = {n : {"time": events[n][0], "name": events[n][1]} for n in range(len(events))}
+
+print(result)
+
+driver.quit()
