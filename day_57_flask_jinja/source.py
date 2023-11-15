4a43bfc6fe5a266a142e8300f61f4b2f29e3e046
Rodrigo Farah <digofarah@gmail.com>
2023-11-15 18:36:41 -0300

    First Commit. Flask and Jinja exercise

---
+++ day_57_flask_jinja/source.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from datetime import datetime
+import requests as rq
+
+GENDER_ENDPOINT = "https://api.genderize.io"
+AGE_ENDPOINT = "https://api.agify.io"
+
+
+app = Flask(__name__)
+
+
+@app.route("/guess/<input_name>")
+def home(input_name):
+    params = {
+        "name": input_name
+    }
+
+    gender_ticket = rq.get(url=GENDER_ENDPOINT, params=params)
+    age_ticket = rq.get(url=AGE_ENDPOINT, params=params)
+    just_gender = gender_ticket.json()["gender"]
+    just_age = age_ticket.json()["age"]
+
+    year = datetime.now().year
+    return render_template("index.html", year=year, name=input_name, gender=just_gender, age=just_age)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
