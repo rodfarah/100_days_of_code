fb42ad98c295a18056946ec8e73f45a24d53b136
Rodrigo Farah <digofarah@gmail.com>
2023-11-29 14:20:44 -0300

    Second Commit. Excited to create a blog page from scratch, even though front end is not my focus. Used Flask, Jinja, Bootstrap (html, css, js) in this exercise. Hope it open doors to create websites when needed.

--- upgraded_blog/main.py
+++ upgraded_blog/main.py
@@ -1,14 +1,36 @@
 from flask import Flask, render_template
+import requests as rq
+
+response = rq.get(url="https://api.npoint.io/79af83afe03463a29f67")
+blog_content = response.json()
+
 
 app = Flask("__main__")
 
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', ap=blog_content)
 
-#TODO: @app.route('/about')
-#def about():
-#    return render_template('')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/post/<int:num>')
+def post(num):
+    requested_post = None
+    for post in blog_content:
+        if post['id'] == num:
+            requested_post = post
+    return render_template('post.html', pst=requested_post)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
